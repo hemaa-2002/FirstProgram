da52369a5d864d5d55437a8c30d98805c41dc405
user id 1 <user id 1@LAPTOP-T8BM8HCL>
2024-02-20 11:37:04 +0530

    Initial Commit.

---
+++ sample/src/com/hema/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.hema;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello world");
+		
+
+	}
+
+}
